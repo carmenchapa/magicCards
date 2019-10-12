972796d466179aa1bc52f36808694067ee417bc6
Carmen Chapa <cmchapa28@gmail.com>
2019-10-12 21:46:55 +0200

    Load fonts with AppLoading

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import {AppLoading} from "expo"
+import * as Font from "expo-font"
+import Home from "./src/Home.js"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    isReady: false
+  }
+
+  _cacheResourcesAsync = () => {
+    const asyncTasks = [
+      Font.loadAsync({
+        MuseoLight: require("./static/fonts/MuseoSans-100.otf"),
+        MuseoRegular: require("./static/fonts/MuseoSans-300.otf"),
+        MuseoMedium: require("./static/fonts/MuseoSans-500.otf"),
+        MuseoBold: require("./static/fonts/MuseoSans-700.otf"),
+        MuseoBlack: require("./static/fonts/MuseoSans-900.otf"),
+        MuseoItalicLight: require("./static/fonts/MuseoSans-100Italic.otf"),
+        MuseoItalicRegular: require("./static/fonts/MuseoSans-300Italic.otf"),
+        MuseoItalicMedium: require("./static/fonts/MuseoSans-500Italic.otf"),
+        MuseoItalicBold: require("./static/fonts/MuseoSans-700Italic.otf"),
+        MuseoItalicBlack: require("./static/fonts/MuseoSans-900Italic.otf")
+      })
+    ]
+
+    return Promise.all(asyncTasks).catch(error => console.log(error))
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({isReady: true})}
+          onError={console.warn}
+        />
+      )
+    }
+
+    return <Home />
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
