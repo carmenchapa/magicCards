490af3dcaaf6258cbedff2d23f4fa2c3e40dbfb8
Carmen Chapa <cmchapa28@gmail.com>
2019-10-13 00:39:44 +0200

    Filter by color

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
 import React from "react"
-import {FlatList, StyleSheet, Text, View} from "react-native"
+import {ActivityIndicator, FlatList, StyleSheet, Text, View} from "react-native"
 import axios from "axios"
 // import mtgsdk from "mtgsdk"
 import {ImageListItem} from "./components/ImageList"
+import {Colors} from "./components/ColorsRow"
 import {styles as s} from "./Styles"
 import {
   widthPercentageToDP as wp,
@@ -12,11 +13,17 @@ import {
 export default class Home extends React.Component {
   state = {
     page: 0,
-    cards: []
+    cards: [],
+    selectedColor: null
   }
   cardsCall = () => {
     console.log("cardsCall", this.state.page)
-    const url = `https://api.magicthegathering.io/v1/cards?contains=imageUrl&pageSize=30&page=${this.state.page}`
+    let url = `https://api.magicthegathering.io/v1/cards?contains=imageUrl&pageSize=30&page=${this.state.page}`
+    url = this.state.selectedColor
+      ? url + `&colors=${this.state.selectedColor.toLowerCase()}`
+      : url
+
+    console.log("url", url)
     axios
       .get(url)
       .then(response => {
@@ -24,6 +31,7 @@ export default class Home extends React.Component {
         return this.setState({
           page: this.state.page + 1,
           cards: this.state.cards.concat(
+            //Filter cards with image, 'contains=imageUrl' does not work for repeated images
             response.data.cards.filter(e => e.imageUrl)
           )
         })
@@ -31,7 +39,36 @@ export default class Home extends React.Component {
       .catch(error => console.log(error))
   }
 
+  resetCards = () => {
+    this.setState({page: 0, cards: []}, this.cardsCall)
+  }
+
   onEndReached = () => this.cardsCall()
+
+  selectColor = c => {
+    console.log(c)
+    this.setState({selectedColor: c}, this.resetCards)
+  }
+
+  headerComponent = () => {
+    const colors = ["White", "Blue", "Green", "Red", "Black"]
+    return (
+      <View
+        style={[
+          {backgroundColor: "white"},
+          s.fullWidth,
+          s.centerContent,
+          s.centerItems
+        ]}
+      >
+        <Colors
+          colors={colors}
+          onPress={c => this.selectColor(c)}
+          selected={this.state.selectedColor}
+        />
+      </View>
+    )
+  }
 
   componentDidMount() {
     this.cardsCall()
@@ -46,11 +83,17 @@ export default class Home extends React.Component {
           renderItem={({item, index}) => (
             <ImageListItem
               item={item}
-              index={index}
-              navigate={() =>
-                navigation.navigate("Detail", {data: "random", index: index})
-              }
+              // navigate={() =>
+              //   navigation.navigate("Detail", {data: "random", index: index})
+              // }
             />
+          )}
+          ListHeaderComponent={this.headerComponent}
+          stickyHeaderIndices={[0]}
+          ListEmptyComponent={() => (
+            <View style={[s.fullFill, s.centerContent, s.centerItems]}>
+              <ActivityIndicator />
+            </View>
           )}
           contentContainerStyle={{
             marginTop: hp("14%"),
