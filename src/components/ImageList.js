8dd1f2e8549c151dcc7a6db3a21433b82544b6a8
Carmen Chapa <cmchapa28@gmail.com>
2019-10-12 21:48:54 +0200

    Add list element

---
+++ src/components/ImageList.js
@@ -0,0 +1,43 @@
+import React from "react"
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native"
+import {LinearGradient} from "expo-linear-gradient"
+import {styles as s} from "../Styles"
+
+export const ImageListItem = props => {
+  return (
+    <TouchableOpacity onPress={() => console.log(props.item)}>
+      <View style={s.cardShadow}>
+        <View style={s.listItem}>
+          <Image
+            style={[s.magicItem]}
+            source={{
+              uri: props.item.imageUrl
+            }}
+            resizeMode="contain"
+          ></Image>
+          <View style={s.info}>
+            <Text style={s.feePhotoTitle}>{`${props.item.name}`}</Text>
+            <Text style={s.feeLikes}>{`${props.item.type}`}</Text>
+            <Text style={s.feeLikes}>{`${props.item.setName}`}</Text>
+            <Colors colors={props.item.colors} />
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const Colors = props => (
+  <View style={s.colors}>
+    {props.colors.map((e, i) => (
+      <View key={i} style={[s.color, {backgroundColor: e.toLowerCase()}]} />
+    ))}
+  </View>
+)
