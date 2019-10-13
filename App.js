c8a4f0970e30cc1c1b6e9692fcc9e6b18676e237
Carmen Chapa <cmchapa28@gmail.com>
2019-10-14 01:48:56 +0200

    Delete unused fonts

--- App.js
+++ App.js
@@ -11,16 +11,9 @@ export default class App extends React.Component {
   _cacheResourcesAsync = () => {
     const asyncTasks = [
       Font.loadAsync({
-        MuseoLight: require("./static/fonts/MuseoSans-100.otf"),
         MuseoRegular: require("./static/fonts/MuseoSans-300.otf"),
-        MuseoMedium: require("./static/fonts/MuseoSans-500.otf"),
         MuseoBold: require("./static/fonts/MuseoSans-700.otf"),
-        MuseoBlack: require("./static/fonts/MuseoSans-900.otf"),
-        MuseoItalicLight: require("./static/fonts/MuseoSans-100Italic.otf"),
-        MuseoItalicRegular: require("./static/fonts/MuseoSans-300Italic.otf"),
-        MuseoItalicMedium: require("./static/fonts/MuseoSans-500Italic.otf"),
-        MuseoItalicBold: require("./static/fonts/MuseoSans-700Italic.otf"),
-        MuseoItalicBlack: require("./static/fonts/MuseoSans-900Italic.otf")
+        MuseoBlack: require("./static/fonts/MuseoSans-900.otf")
       })
     ]
 
